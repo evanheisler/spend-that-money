8d560d8b159badbedeab8263cd2b22134d6b06e5
Evan Heisler <evanheisler@gmail.com>
2019-08-20 09:23:29 -0500

    Updates

---
+++ precache-manifest.b556682a4d36f1b46cd1d4d5750c13d4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17fbe0cf12e1d1d29662e8ba1c626aa6",
+    "url": "/spend-that-money/index.html"
+  },
+  {
+    "revision": "ad9f66526be4c3e173c3",
+    "url": "/spend-that-money/static/css/main.ea861131.chunk.css"
+  },
+  {
+    "revision": "9ca32316f59449594604",
+    "url": "/spend-that-money/static/js/2.0c62b50e.chunk.js"
+  },
+  {
+    "revision": "ad9f66526be4c3e173c3",
+    "url": "/spend-that-money/static/js/main.6711ec20.chunk.js"
+  },
+  {
+    "revision": "6e5c1137e10378c079bd",
+    "url": "/spend-that-money/static/js/runtime~main.e0b2dd84.js"
+  }
+]);
